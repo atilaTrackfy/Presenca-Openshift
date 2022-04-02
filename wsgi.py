f6dd7f58f52337aca132bf1730c9479ad0dab00c
felix-farias-mp <felix.farias@trackfyapp.com>
2022-04-02 11:13:44 -0300

    fix: adjusting to_sql param name

--- wsgi.py
+++ wsgi.py
@@ -91,7 +91,7 @@ def calculatePresence():
             tableProcData             = tableRawData[ ['ts', 'beacon'] ]
             tableProcData.join(locationDtFrame)
             #tableProcData.loc['scanner'] = location
-            tableProcData.to_sql('local', conn, index=False, if_exists='append', chucksize=1000)
+            tableProcData.to_sql('local', conn, index=False, if_exists='append', chunksize=1000)
 
         return isThereData
     return conn
